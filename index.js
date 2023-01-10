a694ae0eeca8a0358d9737ff3ae7b1e83d8ffe4e
Nightowlam <nightowl3.00am@gmail.com>
2023-01-10 08:21:03 +0600

    update api created

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,7 +9,7 @@ app.use(express.json());
 
 // MongoDB Connection
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1lp5l5h.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -39,8 +38,34 @@ const dbConnect = async (req, res) => {
       res.send(result);
     });
 
+    //
     app.get("/people", async (req, res) => {
       const result = await resourceCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    //
+    app.get("/people/:id", async (req, res) => {
+      const query = { _id: ObjectId(req.params.id) };
+      const result = await resourceCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Update Table row data
+    app.put("/people/:id", async (req, res) => {
+      const id = req.params.id;
+      const resource = req.body;
+      const query = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          firstName: resource.firstName,
+          lastName: resource.lastName,
+          age: resource.age,
+          email: resource.email,
+        },
+      };
+      const result = await resourceCollection.updateOne(query, updateDoc, options);
       res.send(result);
     });
   } finally {
@@ -50,6 +75,7 @@ dbConnect().catch((error) => console.log(error.message));
 
 app.get("/", (req, res) => {
   res.send("Server is running");
+  ``;
 });
 
 app.listen(port, () => {
